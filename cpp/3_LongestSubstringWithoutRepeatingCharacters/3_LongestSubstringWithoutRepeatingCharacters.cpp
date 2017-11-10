1f99cfb06dcfd4a1cc7977dd177821231e6094b5
randy <randycma@gmail.com>
2017-11-10 09:27:20 -0800

    second solution, much faster but can still do better

--- cpp/3_LongestSubstringWithoutRepeatingCharacters/3_LongestSubstringWithoutRepeatingCharacters.cpp
+++ cpp/3_LongestSubstringWithoutRepeatingCharacters/3_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -29,24 +29,37 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int longestLength = 0;
-        std::unordered_set<int> lettersSeen;
 
-        for(string::iterator it = s.begin(); it != s.end(); ++it)
+        std::unordered_set<int> lettersSeen;
+        string::iterator it = s.begin();
+        string::iterator it2 = s.begin();
+        int len = 0;
+
+        for(; it != s.end(); ++it)
         {
-        	lettersSeen.clear();
-        	// find longest substring without repeating character for this index
-        	int len = 0;
-        	string::iterator it2 = it;
-        	while(it2 != s.end() && lettersSeen.find(*it2) == lettersSeen.end())
+        	if(lettersSeen.find(*it2) == lettersSeen.end())
         	{
-        		lettersSeen.insert(*it2);
-        		++len;
-        		++it2;
+        		// continue searching
+        		while(it2 != s.end() && lettersSeen.find(*it2) == lettersSeen.end())
+				{
+					lettersSeen.insert(*it2);
+					++len;
+					++it2;
+				}
+
+        		// exited due to duplicate or end of string. check if longer than current longest
+        		if(len > longestLength)
+        		{
+        			longestLength = len;
+        		}
+
+
         	}
-        	if(len > longestLength)
-        	{
-        		longestLength = len;
-        	}
+
+			// remove letter at first pointer from set, decrement length.
+			--len;
+			lettersSeen.erase(*it);
+			continue;
         }
         return longestLength;
     }
@@ -56,7 +69,7 @@ public:
 
 int main()
 {
-	std::string str("kvaemuioeapmaut");
+	std::string str("abcabcbbnauewb");
 	Solution s;
 	cout << "Longest substring for " << str << " is " << s.lengthOfLongestSubstring(str) << std::endl;
 	return 0;
