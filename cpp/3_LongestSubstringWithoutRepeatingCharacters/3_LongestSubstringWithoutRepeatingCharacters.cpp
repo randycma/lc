812a5f43f79435cf14ed8a8aed31ca3b53960ae7
randy <randycma@gmail.com>
2017-11-10 17:22:18 -0800

    replace hashmap with vector lookup using ascii

--- cpp/3_LongestSubstringWithoutRepeatingCharacters/3_LongestSubstringWithoutRepeatingCharacters.cpp
+++ cpp/3_LongestSubstringWithoutRepeatingCharacters/3_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -23,6 +23,7 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
 #include <iostream>
 #include <unordered_set>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
@@ -31,30 +32,16 @@ public:
     int lengthOfLongestSubstring(string s) {
         int longestLength = 0;
 
-        std::unordered_map<int,int> letterSeenToIdx;
+        std::vector<int> letterSeenToIdx(256,-1); // ascii to index
         int i = 0;
-        int j = 0;
 
         // use i and j to mark longest substring currently seen
-
-        for(; j < s.size(); ++j)
+        for(int j = 0; j < s.size(); ++j)
         {
-        	auto it = letterSeenToIdx.find(s[j]);
-        	if(it != letterSeenToIdx.end())
-        	{
-        		// letter already seen, check to see if it's inside our current substring
-        		if(it->second >= i)
-        		{
-        			i = it->second + 1;
-        		}
-        	}
-
+        	i = max(i, letterSeenToIdx[s[j]] + 1); // update i if letter already seen in current substring
         	letterSeenToIdx[s[j]] = j; // update last seen
 
-        	if( (j-i + 1) > longestLength)
-        	{
-        		longestLength = j-i + 1;
-        	}
+        	longestLength = max(j-i+1, longestLength);
         }
         return longestLength;
     }
@@ -65,7 +52,7 @@ public:
 
 int main()
 {
-	std::string str("abca");
+	std::string str("iwefoweobb");
 	Solution s;
 	cout << "Longest substring for " << str << " is " << s.lengthOfLongestSubstring(str) << std::endl;
 	return 0;
